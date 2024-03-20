a1e253abf284d0d998d9a9e9ae88633677080060
Neilshake <pedroonline0311@gmail.com>
2024-03-20 17:03:48 -0300

    first commit

---
+++ src/br/com/plopes/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+package br.com.plopes.dao;
+
+import br.com.plopes.domain.Cliente;
+
+import java.util.Collection;
+
+
+public interface IClienteDAO {
+    public Boolean cadastrar(Cliente cliente);
+    public void excluir(Long cpf);
+    public void alterar(Cliente cliente);
+    public Cliente consultar(Long cpf);
+    public Collection<Cliente> buscarTodos();
+
+
+}
